eabd20c8eebfab39fa925ead6b208a3992ad1bb4
Micaella Colombo <micaelc@g.clemson.edu>
2022-01-13 19:05:44 -0500

    Done.

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,12 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root 'users#new'
+
+  resources :users, only: [:new, :create, :show]
+  resources :attractions
+  resources :rides, only: [:new, :create]
+
+  get '/signin' => 'sessions#new'
+  post '/signin' => 'sessions#create'
+  get '/signout', to: 'sessions#destroy', via: 'delete'
 end
